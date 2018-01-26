d3670073d88f831ddd526d31dcceb634acd8f90c
Dave van Vugt <dave.van.vugt@devoteam.nl>
2018-01-26 21:28:37 +0100

    Map renderer voor iOS

---
+++ TimesheetGPS/TimesheetGPS.iOS/CustomMapRenderer_iOS.cs
@@ -0,0 +1,127 @@
+﻿using CoreGraphics;
+using MapKit;
+using System;
+using System.Collections.Generic;
+using TimesheetGPS.iOS;
+using TimesheetGPS.Model;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+using Xamarin.Forms.Maps.iOS;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(CustomMap), typeof(CustomMapRenderer))]
+namespace TimesheetGPS.iOS
+{
+    public class CustomMapRenderer : MapRenderer
+    {
+        UIView customPinView;
+        List<CustomPin> customPins;
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Xamarin.Forms.View> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.OldElement != null)
+            {
+                var nativeMap = Control as MKMapView;
+                if (nativeMap != null)
+                {
+                    nativeMap.RemoveAnnotations(nativeMap.Annotations);
+                    nativeMap.GetViewForAnnotation = null;
+                    nativeMap.CalloutAccessoryControlTapped -= OnCalloutAccessoryControlTapped;
+                    nativeMap.DidSelectAnnotationView -= OnDidSelectAnnotationView;
+                    nativeMap.DidDeselectAnnotationView -= OnDidDeselectAnnotationView;
+                }
+            }
+
+            if (e.NewElement != null)
+            {
+                var formsMap = (CustomMap)e.NewElement;
+                var nativeMap = Control as MKMapView;
+                customPins = formsMap.CustomPins;
+
+                nativeMap.GetViewForAnnotation = GetViewForAnnotation;
+                nativeMap.CalloutAccessoryControlTapped += OnCalloutAccessoryControlTapped;
+                nativeMap.DidSelectAnnotationView += OnDidSelectAnnotationView;
+                nativeMap.DidDeselectAnnotationView += OnDidDeselectAnnotationView;
+            }
+        }
+
+        MKAnnotationView GetViewForAnnotation(MKMapView mapView, IMKAnnotation annotation)
+        {
+            MKAnnotationView annotationView = null;
+
+            if (annotation is MKUserLocation)
+                return null;
+
+            var customPin = GetCustomPin(annotation as MKPointAnnotation);
+            if (customPin == null)
+            {
+                throw new Exception("Custom pin not found");
+            }
+
+            annotationView = mapView.DequeueReusableAnnotation(customPin.Label);
+            if (annotationView == null)
+            {
+                annotationView = new MKAnnotationView(annotation, customPin.Label);
+                annotationView.Image = UIImage.FromFile("pin.png");
+                annotationView.CalloutOffset = new CGPoint(0, 0);
+                //annotationView.LeftCalloutAccessoryView = new UIImageView(UIImage.FromFile("monkey.png"));
+                //annotationView.RightCalloutAccessoryView = UIButton.FromType(UIButtonType.DetailDisclosure);
+            }
+            annotationView.CanShowCallout = true;
+
+            return annotationView;
+        }
+
+        void OnDidSelectAnnotationView(object sender, MKAnnotationViewEventArgs e)
+        {
+            var customView = e.View as MKAnnotationView;
+            customPinView = new UIView();
+
+            //if (customView.Id == "Xamarin")
+            //{
+            //    customPinView.Frame = new CGRect(0, 0, 200, 84);
+            //    var image = new UIImageView(new CGRect(0, 0, 200, 84));
+            //    image.Image = UIImage.FromFile("xamarin.png");
+            //    customPinView.AddSubview(image);
+            //    customPinView.Center = new CGPoint(0, -(e.View.Frame.Height + 75));
+            //    e.View.AddSubview(customPinView);
+            //}
+        }
+
+        void OnCalloutAccessoryControlTapped(object sender, MKMapViewAccessoryTappedEventArgs e)
+        {
+            var customView = e.View as MKAnnotationView;
+            //if (!string.IsNullOrWhiteSpace(customView.Url))
+            //{
+            //    UIApplication.SharedApplication.OpenUrl(new Foundation.NSUrl(customView.Url));
+            //}
+        }
+
+        void OnDidDeselectAnnotationView(object sender, MKAnnotationViewEventArgs e)
+        {
+            if (!e.View.Selected)
+            {
+                customPinView.RemoveFromSuperview();
+                customPinView.Dispose();
+                customPinView = null;
+            }
+        }
+
+        private CustomPin GetCustomPin(MKPointAnnotation annotation)
+        {
+            var position = new Position(annotation.Coordinate.Latitude, annotation.Coordinate.Longitude);
+            foreach (var pin in customPins)
+            {
+                if (pin.Position == position)
+                {
+                    return pin;
+                }
+            }
+            return null;
+        }
+
+    }
+}
